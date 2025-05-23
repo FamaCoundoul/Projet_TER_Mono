fb08b9e1100291416b25f733d8c27ba9349c3bbe
njap <njap@njaps-MacBook-Pro.local>
2025-05-23 20:07:38 +0200

    Gestion des dependances des conteneurs

---
+++ build_and_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "✅ Compilation Maven..."
+cd backend-mono/E-Commerce
+mvn clean package -DskipTests
+cd -
+
+echo "📦 Copie du .jar dans le dossier racine..."
+cp backend-mono/E-Commerce/target/E-Commerce-0.0.1-SNAPSHOT.jar .
+
+echo "🐳 Build Docker..."
+docker-compose build backend
+
+echo "🚀 Lancement..."
+docker-compose up
